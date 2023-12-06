918ba22ab96b45c63749a1cd65fef1d6a2db4ecd
180cmus <impbpb.icloud.com>
2023-12-06 10:03:48 +0700

    Add Next/Prev Button

--- src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -1,24 +1,65 @@
 import React from 'react'
 import AliceCarousel from 'react-alice-carousel'
 import HomeSectionCard from '../HomeSectionCard/HomeSectionCard'
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowLeft';
+import { Button } from '@mui/material';
 
 const responsive = {
-    0: { items: 1},
+    0: { items: 1 },
     720: { items: 3 },
-    1024: { items: 5.5}
+    1024: { items: 5.5 }
 }
-const items = [1,1,1,1,1].map(item => <HomeSectionCard/>)
+const items = [1, 1, 1, 1, 1].map(item => <HomeSectionCard />)
 const HomeSectionCarousel = () => {
     return (
-        <div>
-            <AliceCarousel
-                items={items}
-                disableButtonsControls
-                autoPlay
-                autoPlayInterval={1000}
-                infinite
-                responsive={responsive}
-            />
+        <div className='relative px-4 sm:px lg:px-8'>
+            <div className='relative p-5 border'>
+                <AliceCarousel
+                    items={items}
+                    disableButtonsControls
+                    infinite
+                    responsive={responsive}
+                    disableDotsControls
+                />
+                <Button 
+                    variant='contained'
+                    className='z-50'
+                    sx={{ 
+                        position: 'absolute',
+                        top: '8rem',
+                        right: '0rem',
+                        transform: 'translateX(50%) rotate(90deg)',
+                        bgcolor: 'white' 
+                    }}
+                    aria-label='next'
+                >
+                    <KeyboardArrowLeftIcon 
+                        sx={{ 
+                            transform: 'rotate(90deg)',
+                            color: 'black' 
+                        }}
+                    />
+                </Button>
+                <Button 
+                    variant='contained'
+                    className='z-50'
+                    sx={{ 
+                        position: 'absolute',
+                        top: '8rem', left: '0rem',
+                        transform: 'translateX(-50%) rotate(90deg)',
+                        bgcolor: 'white' 
+                    }}
+                    aria-label='next'
+                >
+                    <KeyboardArrowRight 
+                        sx={{ 
+                            transform: 'rotate(-90deg)',
+                            color: 'black' 
+                        }}
+                    />
+                </Button>
+            </div>
         </div>
     )
 }
--- src/customer/pages/Homepage/Homepage.jsx
+++ src/customer/pages/Homepage/Homepage.jsx
@@ -7,7 +7,11 @@ const Homepage = () => {
     return (
         <div>
             <MainCarousel/>
-            <div>
+            <div className='space-y-10 py-20'>
+                <HomeSectionCarousel/>
+                <HomeSectionCarousel/>
+                <HomeSectionCarousel/>
+                <HomeSectionCarousel/>
                 <HomeSectionCarousel/>
             </div>
         </div>
