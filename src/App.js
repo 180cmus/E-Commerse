9e364ada57cf0e1d2f633926a8822b8f956ffb95
180cmus <impbpb.icloud.com>
2023-12-10 10:55:14 +0700

    Product Card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Footer from './customer/components/Footer/Footer';
 import Navigation from './customer/components/Navigation/Navigation';
+import Product from './customer/components/Product/Product';
 import Homepage from './customer/pages/Homepage/Homepage';
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
     <div className="">
       <Navigation/>
       <div>
-        <Homepage/>
+        {/* <Homepage/> */}
+        <Product/>
       </div>
       <Footer/>
     </div>
